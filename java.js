d7ad4e4db7e0db30a4fffaf0a4ada9ed2f89b56f
wanjiruwainaina <maryannewainaina61@gmail.com>
2024-05-13 17:45:38 +0300

    add

---
+++ java.js
@@ -0,0 +1,7 @@
+{   let t = 20;
+    let b = 40;
+    let h = 16;
+    let a = (t+b)*h/2;
+    console.log(a);
+
+}
---
+++ practice.js
@@ -0,0 +1,25 @@
+
+let x = 50
+{
+    console.log(x);
+    const  g = 9.98;
+    console.log(g);
+    
+}
+{   console.log(x);
+    const pi = 3.142;
+    console.log (pi);
+
+ }
+ {  let l = 7;
+    let w = 8;
+    let a = w*l;
+    console.log(a);
+ }
+ {
+    const pi=3.142;
+    let d = 64;
+    let r = d/2;
+    let a = pi*r**2;
+    console.log(a);
+ }
