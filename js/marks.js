c142fa4192a36c0586f205c4162f671a914143ef
wanjiruwainaina <maryannewainaina61@gmail.com>
2024-05-19 19:39:46 +0300

    add

---
+++ js/marks.js
@@ -0,0 +1,50 @@
+{function calculateAverageMarks(arrayOfStudents) {
+
+    arrayOfStudents.forEach(student) 
+        {
+            const results = [
+                {name: "Stephen",
+                marks: [45, 78, 90, 32, 67]},
+                {name: "Maryanne",
+                marks: [89, 78, 10, 67, 47]},
+                {name: "Kelvin",
+                 marks: [55, 88, 94, 52, 37]},
+                {name: "Claire",
+                 marks: [44, 68, 88, 62, 77]}]
+        {const average = student.marks((sum(student.marks.length)) / student.marks.length)
+        student.average = average;}
+    };
+    arrayOfStudents.sort((a,b),b.average - a.average);
+
+
+    arrayOfStudents.forEach(student ) 
+        console.log([student.name],[student.average])
+    };
+};
+
+
+calculateAverageMarks(arrayOfStudents);
+
+const numbers=[1,2,3,4,5] 
+    console.log(number[5])
+
+// {
+//     const results = [
+//      {
+//          name: "Jane",
+//          marks: [89, 45, 67, 89, 67]
+//      },
+//      {
+//          name: "Alfred",
+//          marks: [89, 56, 43, 78, 89]
+//      },
+//      {
+//          name:"Steve",
+//          marks: [67, 89, 56, 44, 90]
+//      }
+//     ]
+//     console.log(results[2].marks[3])
+//     console.log(results[1].name + ": " + results[1].marks[3])
+//  }
+ 
+ 
