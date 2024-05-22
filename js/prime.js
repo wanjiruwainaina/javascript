1e3da8f178ce798ef7154decc2657da1968ad233
wanjiruwainaina <maryannewainaina61@gmail.com>
2024-05-22 08:32:53 +0300

    add

--- js/prime.js
+++ js/prime.js
@@ -1,21 +1,4 @@
-// function isPrime(n)
-// let n = 50 {
-//     if (n <= 1) {  
-//         return false;
-//     } else if (n <= 3) {  
-//         return true;
-//     } else if (n % 2 === 0 || n % 3 === 0) {  
-//         return false;
-//     }
-//     let i = 5;
-//     while (i * i <= n) {  
-//         if (n % i === 0 || n % (i + 2) === 0) {  
-//             return false;
-//         }
-//         i += 6;  
-//     }
-//     return true;
-// }
+
 {function GetPrimeNumbers(n){
     let i = 1; i <=50; i++
     if(n % i ==0){
